999d7d7a52b631005e39a427fce98f049062ce49
achaur11 <achaur11@asu.edu>
2024-07-06 19:38:56 -0700

    Removing Pydantic reference from Knowledge graph

--- server/knowledge_graph/knowledge_graph.py
+++ server/knowledge_graph/knowledge_graph.py
@@ -18,7 +18,6 @@ class KnowledgeGraph:
         self.app.add(f"SELECT key, explanation, inference FROM inference WHERE project_id={project_id};", data_type='postgres', loader=self.postgres_loader, metadata={"project_id": project_id})
         self.app.add(f"SELECT * FROM endpoints WHERE project_id={project_id};", data_type='postgres', loader=self.postgres_loader, metadata={"project_id": project_id})
         self.app.add(f"SELECT identifier, explanation FROM explanation WHERE project_id={project_id};", data_type='postgres', loader=self.postgres_loader, metadata={"project_id": project_id})
-        self.app.add(f"SELECT * FROM pydantic WHERE project_id={project_id};", data_type='postgres', loader=self.postgres_loader, metadata={"project_id": project_id})
 
     def query(self, query, project_id):
         prefix = "Always INCLUDE ALL RELEVANT FILEPATH, FUNCTION NAME AND VARIABLE NAMES in your response. If you are asked about an API: ALWAYS include its HTTP verb and url path along with its identifier in the response: \n"
