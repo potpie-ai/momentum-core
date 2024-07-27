944e583a2432338050b0193836fc2b03858f37b7
Vineet <vn.sh@outlook.com>
2024-07-28 03:57:09 +0530

    make kg run locally so it picks up local git repo

--- start_momentum.sh
+++ start_momentum.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-# Bring up all services except momentum
+# Bring up all services except momentum and knowledge_graph
 docker-compose up -d
 
-# Wait for postgres and knowledge-graph to be ready
-echo "Waiting for postgres and knowledge_graph to be ready..."
-until docker inspect -f {{.State.Health.Status}} postgres | grep -q "healthy" && docker inspect -f {{.State.Status}} knowledge_graph | grep -q "running"; do
+# Wait for postgres to be ready
+echo "Waiting for postgres to be ready..."
+until docker inspect -f {{.State.Health.Status}} postgres | grep -q "healthy"; do
   sleep 5
 done
+
+# Change to knowledge_graph directory
+cd knowledge_graph
+
+# Start celery worker and main application
+echo "Starting Celery worker..."
+celery --app=inferflow worker -l INFO --pool solo -Q ${CELERY_QUEUE_NAME} &
+
+echo "Starting main application..."
+python3 main.py &
+
+# Return to the original directory
+cd ..
 
 # Run momentum application with migrations
 echo "Starting momentum application..."
